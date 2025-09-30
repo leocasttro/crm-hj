5ed61be84902c2c3d45edd1fa7611b58f66eef4a
leocasttro <leocasttro01@icloud.com>
2025-09-30 00:09:53 -0300

    feature navBar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from "./shared/nav-bar/nav-bar";
+import { SideBar } from "./shared/side-bar/side-bar";
+import { Pedidos } from "./feature/pedidos/pedidos";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBar, SideBar, Pedidos],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
